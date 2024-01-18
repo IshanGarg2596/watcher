2a82b6f0d80c3f27d4db5a798bcf008c9f0793ef
ishan.garg2596@gmail.com <ishan.garg2596@gmail.com>
2024-01-18 22:09:33 +0530

    add Product type

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Watcher
 
 Ecommerce website for a Smart Watch store.
+
+## Steps for a creating mern project with TypeScript:
+
+    1. install vscode extensions: es7 + react, prettier, eslint
+    2. Make frontend of the project with vite and select react + typescript.
+    3. Make folder structure add pages, components and types folders and add other folders as required.
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,28 @@
-import './App.css'
+import './App.css';
+import { sampleProducts } from './data.ts';
 
 function App() {
   return (
     <div>
       <header>Watcher</header>
-      <main> Hello Watch lovers!</main>
+      <main>
+        <ul>
+          {sampleProducts.map((product) => (
+            <li key={product.slug}>
+              <img
+                src={product.image}
+                alt={product.name}
+                className="product-image"
+              ></img>
+              <h2>{product.name}</h2>
+              <p>${product.price}</p>
+            </li>
+          ))}
+        </ul>
+      </main>
       <footer>All rights reserved.</footer>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
