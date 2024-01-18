3dc3601cd120755f1f59c31a25a12b9a3bda74c8
ishan.garg2596@gmail.com <ishan.garg2596@gmail.com>
2024-01-18 20:43:47 +0530

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <header>Watcher</header>
+      <main> Hello Watch lovers!</main>
+      <footer>All rights reserved.</footer>
+    </div>
+  )
+}
+
+export default App
