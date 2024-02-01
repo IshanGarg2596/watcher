1d7787a256ee7b5b7e1d9e8ad23a1d2ec82c0775
ishan.garg2596@gmail.com <ishan.garg2596@gmail.com>
2024-02-02 00:28:50 +0530

    Create context for mode

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -16,6 +16,7 @@ import Product from './pages/Product.tsx';
 import { HelmetProvider } from 'react-helmet-async';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { StoreProvider } from './store.tsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -32,11 +33,13 @@ const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
-    </HelmetProvider>
+    <StoreProvider>
+      <HelmetProvider>
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+      </HelmetProvider>
+    </StoreProvider>
   </React.StrictMode>
 );
